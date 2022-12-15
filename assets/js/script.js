ef3d37d21db6771e893b5c5add8ee11ea09fc01a
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2022-12-15 11:08:43 +0800

    added placeholder text in html for better user experience, updated the current time display to immediately show, and cleaned up js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,49 @@
 
 $(function () {
+  // Display the updated current date and time in the header of the page immediately.
+  currentTime()
 
-  // Display the updated current date and time in the header of the page.
-  setInterval(function() {
+  function currentTime() {
     const today = dayjs();
-    $("#currentDay").text(today.format("dddd, DD MMMM YYYY, h:mm A"));
-  },1000)
+    $("#currentDay").text(today.format("dddd, DD MMMM YYYY, h:mm:ss A"));
+  }
+
+  const timeUpdate = setInterval(currentTime, 1000);
 
   // Clears local storage when button is clicked
   $("#clear-btn").on("click", function() { 
-    localStorage.clear()
-    location.reload()
-  })
+    localStorage.clear();
+    location.reload();
+  });
 
   // Runs the main function
   eachBlockStatus()
 
   function eachBlockStatus() {
     // Gets the current hour in 24h time
-    const currentHour = dayjs().hour()
-    let timeBlockEl = $(".time-block")
+    const currentHour = dayjs().hour();
+    let timeBlockEl = $(".time-block");
     $(timeBlockEl).each(function() {
       // For each timeblock it gets the id and removes hour- leaving only the timeblock time
-      let eachBlockTime = Number($(this).attr("id").split("hour-")[1])
+      let eachBlockTime = Number($(this).attr("id").split("hour-")[1]);
       // Adds classes for past, future and present comparing the time to the timeblock
       if (eachBlockTime < currentHour) {
         // Sets colour to grey
-        $(this).addClass("past")
+        $(this).addClass("past");
       } else if (eachBlockTime === currentHour) {
         // Sets colour to red
-        $(this).addClass("present")
+        $(this).addClass("present");
         // Sets colour to green
       } else {
-        $(this).addClass("future")
+        $(this).addClass("future");
       }
-      // Puts what is in already stored in local storage in the text boxes if there is anything.
-      let blockTimeId = $(this).attr("id")
-      let existingNote = localStorage.getItem(blockTimeId)
+      // Puts what is already stored in local storage in the text boxes if there is anything.
+      let blockTimeId = $(this).attr("id");
+      let existingNote = localStorage.getItem(blockTimeId);
       if (existingNote !== null) {
-        $(this).children(".description").val(existingNote)
+        $(this).children(".description").val(existingNote);
       }
-    })
+    });
   }
 
   // When the save button is clicked, it gets the time and text of the box it is connected with and saves to local storage
@@ -48,5 +51,5 @@ $(function () {
     let timeId = $(this).parent().attr("id");
     let textEl = $(this).siblings(".description").val();
     localStorage.setItem(timeId, textEl);
-  })
+  });
 });
